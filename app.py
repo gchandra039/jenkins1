eb9a422348952fb8032429343c1d813fe772835c
gchandra039 <107829933+gchandra039@users.noreply.github.com>
2024-06-17 23:54:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Welcome to Chandr Youtube Channel"
+    return "Welcome to hello Youtube Channel"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
